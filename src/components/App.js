443d4060da16154aa9692f75dc205380ef19dd99
avong4 <avong4@my.bcit.ca>
2019-06-23 14:43:21 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+const App = () => {
+    return (
+    <div>
+        <SearchBar/>
+        <ImageList/>
+    </div>);
+}
+
+export default App;
